936f594c003e19f079cbcb92803443f0fac66189
Lance Judan <aperture.phoenix@gmail.com>
2017-09-10 19:44:11 -0500

    toggle contact selection

--- src/category/CreateCategoryForm.java
+++ src/category/CreateCategoryForm.java
@@ -48,6 +48,27 @@ public class CreateCategoryForm implements ActionListener{
     private void createUIComponents() {
         contactModel = new DefaultListModel<>();
         contactList = new JList<>(contactModel);
-        contactList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        contactList.setSelectionModel(new DefaultListSelectionModel() {
+            boolean gestureStarted = false;
+
+            @Override
+            public void setSelectionInterval(int index0, int index1) {
+                if (!gestureStarted) {
+                    if (isSelectedIndex(index0)) {
+                        super.removeSelectionInterval(index0, index1);
+                    } else {
+                        super.addSelectionInterval(index0, index1);
+                    }
+                }
+                gestureStarted = true;
+            }
+
+            @Override
+            public void setValueIsAdjusting(boolean isAdjusting) {
+                if (isAdjusting == false) {
+                    gestureStarted = false;
+                }
+            }
+        });
     }
 }
